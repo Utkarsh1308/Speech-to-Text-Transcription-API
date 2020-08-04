e10b4b5d8e244d59b6ad9debb152581a3527f499
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2020-08-04 21:14:46 +0530

    Second Commit

--- script.js
+++ script.js
@@ -7,9 +7,31 @@ var diagnostic = document.querySelector('.output');
 var area = document.querySelector('#textarea');
 var icon = document.querySelector('.fa')
 
-icon.onclick = function() {
+var start = document.querySelector('.start')
+var stop = document.querySelector('.stop')
+
+var recognizing = false;
+/*
+
+recognition.onstart = function() {
+    recognizing = true;
+};
+
+recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
+    }
+};
+
+start.onclick = function() {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
   recognition.start();
-  console.log('Ready to receive text.');
+  ignore_onend = false;
+  // console.log('Ready to receive text.');
 }
 
 recognition.onresult = function(event) {
@@ -26,10 +48,99 @@ recognition.onresult = function(event) {
   area.textContent = area.textContent + " " + text;
 }
 
-recognition.onspeechend = function() {
+stop.onclick = function() {
   recognition.stop();
 }
 
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
+
+*/
+
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  start_button.style.display = 'inline-block';
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onstart = function() {
+    recognizing = true;
+  };
+
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      start_img.src = 'mic.gif';
+      showInfo('info_no_speech');
+      ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+      start_img.src = 'mic.gif';
+      showInfo('info_no_microphone');
+      ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        showInfo('info_blocked');
+      } else {
+        showInfo('info_denied');
+      }
+      ignore_onend = true;
+    }
+  };
+
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
+    }
+    start_img.src = 'mic.gif';
+    if (!final_transcript) {
+      showInfo('info_start');
+      return;
+    }
+    showInfo('');
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(document.getElementById('final_span'));
+      window.getSelection().addRange(range);
+    }
+    if (create_email) {
+      create_email = false;
+      createEmail();
+    }
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    area.innerHTML = final_transcript;
+    if (final_transcript || interim_transcript) {
+      showButtons('inline-block');
+    }
+  };
+}
+
+function startButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.start();
+  ignore_onend = false;
+  start_timestamp = event.timeStamp;
+}
+
+stop.onclick = function() {
+  recognition.stop();
 }
